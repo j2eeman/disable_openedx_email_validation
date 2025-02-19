1c5f96bb9825fbf189a15e44b4d603c3e2112d65
DONG QIAN <qdong@cn.ibm.com>
2025-02-19 13:53:27 +0800

    Add include in find_packages()

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='disable-openedx-email-validation',
     version='0.1.0',
     description='Disable email validation in Open edX',
-    packages=find_packages(),
+    packages=find_packages(include=['disable_openedx_email_validation*']),
     include_package_data=True,  # This is crucial!
     package_data={
         'disable_openedx_email_validation': ['hooks.py'],
