1ea7038de2d6f14a087f19cfc00c5a1aab0feee7
DONG QIAN <qdong@cn.ibm.com>
2025-02-19 11:28:49 +0800

    add pakage_data

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1.0',
     description='Disable email validation in Open edX',
     packages=find_packages(),
+    include_package_data=True,  # This is crucial!
+    package_data={
+        'disable_openedx_email_validation': ['hooks.py'],
+    },
     entry_points={
         'tutor.plugins': [
             'disable_openedx_email_validation = disable_openedx_email_validation.hooks',
